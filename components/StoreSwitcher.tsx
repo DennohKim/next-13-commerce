2056568b1181b2fd5715f384a76dd9949612b905
Dennis Kimathi <Kimathidennis38@gmail.com>
2023-08-20 15:15:33 +0300

    feat: create settings page

--- components/StoreSwitcher.tsx
+++ components/StoreSwitcher.tsx
@@ -33,6 +33,7 @@ export default function StoreSwitcher({ className, items = [] }: StoreSwitcherPr
   const params = useParams();
   const router = useRouter();
 
+
   const formattedItems = items.map((item) => ({
     label: item.name,
     value: item.id
